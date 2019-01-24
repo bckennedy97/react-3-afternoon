468c6d4d5b170116366fff3d72292c8a2a172981
Brenden Kennedy <bckennedy97>
2019-01-23 22:48:50 -0700

    completed assignment

--- src/components/Compose/Compose.js
+++ src/components/Compose/Compose.js
@@ -21,7 +21,12 @@ export default class Compose extends Component {
   }
 
   createPost() {
-
+    const {createPostFn} = this.props;
+    const {text} = this.state;
+    createPostFn(text)
+    this.setState({
+      text: ""
+    })
   }
 
   render() {
